660acc8086c022e53dbcd72c3f02dffbc52e16ed
Cecily Grossmann <grssmnn2@gmail.com>
2018-03-04 13:01:06 -0600

    initial server and mysql table setup

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name, devoured) VALUES ('Bleu Cheese Double Bacon Burger with Avocado', true);
+INSERT INTO burgers (name, devoured) VALUES ('Mushroom Burger with Feta Cheese and Sliced Tomato', true);
+INSERT INTO burgers (name, devoured) VALUES ('Triple Cheese Bacon Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Dragon Burger with Ghost Pepper Jelly', true);
