d0a2526a13831049fdcd080f8a69b8b653d53dab
Cecily Grossmann <grssmnn2@gmail.com>
2018-03-09 21:21:12 -0600

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,19 @@
 var mysql = require("mysql");
 var connection;
 
-if(process.env.JAWSDB_URL){
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-  }
-  else{
+if (process.env.JAWSDB_URL) {
+
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+
+} else {
+
     connection = mysql.createConnection({
         host: "localhost",
-        database: "burgers_db",
         user: "root",
-        password: "Caggac11*",
-        port: 3306
-        });
-  }
-
+        password: "",
+        port: 3306,
+        database: "burgers_db"
+    })
+}
+connection.connect();
 module.exports = connection;
